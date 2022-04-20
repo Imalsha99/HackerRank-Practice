db562d1a08086a4548e5ea317b455293a4a3648e
Imalsha Liyanage <64301764+Imalsha99@users.noreply.github.com>
2022-04-20 21:05:50 +0530

    Update Java Stdin and Stdout.java

--- Java Stdin and Stdout.java
+++ Java Stdin and Stdout.java
@@ -2,22 +2,20 @@ import java.util.*;
 
 public class Solution {
     
- public static void main(String[] args) {
-Scanner sc= new Scanner (System.in);
+        public static void main(String[] args) {
+        Scanner sc= new Scanner (System.in);
 
-int num1 = sc.nextInt();
+                int num1 = sc.nextInt();
 
-int num2 = sc.nextInt();
+                int num2 = sc.nextInt();
 
-int num3 = sc.nextInt();
+                int num3 = sc.nextInt();
 
-System.out.println(num1);
-System.out.println(num2);
-System.out.println(num3);
+                System.out.println(num1);
+                System.out.println(num2);
+                System.out.println(num3);
 
-
-   
-}
+         }
    
 }
 
